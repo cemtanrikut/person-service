b455c395fbf9404bb2460107b73d2449f4dbccb8
Cem Tanrikut <cemtanrikut@gmail.com>
2024-09-09 12:55:24 +0200

    add dynamodb config

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module person-service
 
 go 1.22.1
+
+require github.com/aws/aws-sdk-go v1.55.5
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- repo/dynamodb.go
+++ repo/dynamodb.go
@@ -1,1 +1,43 @@
 package repo
+
+import (
+	"context"
+	"person-service/model"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+// Interfaces
+type PersonRepository interface {
+	CreatePerson(ctx context.Context, person model.Person) error
+}
+
+type DynamoDBRepository struct {
+	client *dynamodb.DynamoDB
+}
+
+// Build new dynamo repo
+func NewDynamoDBRepository() *DynamoDBRepository {
+	sess := session.Must(session.NewSession())
+	return &DynamoDBRepository{
+		client: dynamodb.New(sess),
+	}
+}
+
+func (repo *DynamoDBRepository) CreatePerson(ctx context.Context, person model.Person) error {
+	av, err := dynamodbattribute.MarshalMap(person)
+	if err != nil {
+		return err
+	}
+
+	input := &dynamodb.PutItemInput{
+		TableName: aws.String("Persons"),
+		Item:      av,
+	}
+
+	_, err = repo.client.PutItemWithContext(ctx, input)
+	return err
+}

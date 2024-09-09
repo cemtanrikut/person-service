d5b7d2b005b47715e247edcf992b84e29bb8adf5
Cem Tanrikut <cemtanrikut@gmail.com>
2024-09-09 13:03:33 +0200

    add lambda expression

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"person-service/handlers"
 
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	handler := handlers.NewPersonHandler()
+	lambda.Start(handler.HandleRequest)
 }

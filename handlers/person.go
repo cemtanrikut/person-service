604190ab8164ae6d5234319616522793926ce6f3
Cem Tanrikut <cemtanrikut@gmail.com>
2024-09-09 12:57:46 +0200

    build repository

--- handlers/person.go
+++ handlers/person.go
@@ -1,1 +1,14 @@
 package handlers
+
+import "person-service/repo"
+
+// Build repo
+type PersonHandler struct {
+	repository repo.PersonRepository
+}
+
+func NewPersonHandler() *PersonHandler {
+	return &PersonHandler{
+		repository: repo.NewDynamoDBRepository(),
+	}
+}

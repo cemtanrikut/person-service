ff6977349223672ebabcec1117c69610004590b4
Cem Tanrikut <cemtanrikut@gmail.com>
2024-09-09 12:49:26 +0200

    add person model

--- model/person.go
+++ model/person.go
@@ -1,1 +1,18 @@
 package model
+
+type Person struct {
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	PhoneNumber string `json:"phone_number"`
+	Address     string `json:"address"`
+}
+
+type PersonRequest struct {
+	HTTPMethod string `json:"httpMethod"`
+	Body       string `json:"body"`
+}
+
+type PersonResponse struct {
+	StatusCode int    `json:"statusCode"`
+	Body       string `json:"body"`
+}

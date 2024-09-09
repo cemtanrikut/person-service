562705cd335e768e0fcc184e0ee3394c3e81ea2d
Cem Tanrikut <cemtanrikut@gmail.com>
2024-09-09 13:01:26 +0200

    add createperson func and handle it

--- handlers/person.go
+++ handlers/person.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "person-service/repo"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"person-service/model"
+	"person-service/repo"
+)
 
 // Build repo
 type PersonHandler struct {
@@ -11,4 +17,28 @@ func NewPersonHandler() *PersonHandler {
 	return &PersonHandler{
 		repository: repo.NewDynamoDBRepository(),
 	}
+}
+
+func (h *PersonHandler) HandleRequest(ctx context.Context, request model.PersonRequest) (model.PersonResponse, error) {
+	switch request.HTTPMethod {
+	case "POST":
+		return h.createPerson(ctx, request)
+	default:
+		return model.PersonResponse{StatusCode: http.StatusBadRequest}, nil
+	}
+}
+
+func (h *PersonHandler) createPerson(ctx context.Context, request model.PersonRequest) (model.PersonResponse, error) {
+	var person model.Person
+	err := json.Unmarshal([]byte(request.Body), &person)
+	if err != nil {
+		return model.PersonResponse{StatusCode: http.StatusBadRequest}, err
+	}
+
+	err = h.repository.CreatePerson(ctx, person)
+	if err != nil {
+		return model.PersonResponse{StatusCode: http.StatusInternalServerError}, err
+	}
+
+	return model.PersonResponse{StatusCode: http.StatusCreated}, nil
 }
